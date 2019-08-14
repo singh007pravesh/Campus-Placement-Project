ecc7322386a0fbe6cd17acaad716fe32590ddb8d
Pravesh Singh <singh007pravesh@gmail.com>
2019-08-14 22:55:53 +0530

    initial commit on 14 aug

---
+++ src/campus_placement_pravesh/Campus_placement_pravesh.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package campus_placement_pravesh;
+
+/**
+ *
+ * @author PR1VESH
+ */
+public class Campus_placement_pravesh {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login m=new Login();
+        m.setVisible(true);
+    }
+    
+}
